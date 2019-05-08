f18d95652867beb9fe59346eb03f1b7814ccb64a
SpQuyt <spquyt@gmail.com>
2019-05-08 20:42:53 +0700

    add Login box, Login title

---
+++ src/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Screen">
+      <div className="Login-box">
+        <header className="Login-title">Đăng nhập</header>
+      </div>
+    </div>
+
+  );
+}
+
+export default Login;
