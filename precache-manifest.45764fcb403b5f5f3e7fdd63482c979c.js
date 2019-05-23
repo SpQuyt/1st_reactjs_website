2baaf586f7b48c417b943e2f4bd81aba5df95ae1
SpQuyt <spquyt@gmail.com>
2019-05-23 17:31:46 +0700

    Updates

---
+++ precache-manifest.45764fcb403b5f5f3e7fdd63482c979c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5433606946a0398e3ca336c73a92e2ec",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "d62b248f2b536153b134",
+    "url": "/myapp/static/css/main.1c056fad.chunk.css"
+  },
+  {
+    "revision": "7e4077c7ae83ff0abb40",
+    "url": "/myapp/static/js/2.96ad1932.chunk.js"
+  },
+  {
+    "revision": "d62b248f2b536153b134",
+    "url": "/myapp/static/js/main.6dd114f1.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
